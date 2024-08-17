991bd4da7ceff324dccb22120f54b4229f42708b
un <mapzine123@naver.com>
2024-08-18 02:02:25 +0900

    #17 - 상수만 따로 파일 작성

---
+++ src/util/constant.js
@@ -0,0 +1,6 @@
+export const articlePath = "http://localhost:8080/api/articles";
+export const userPath = "http://localhost:8080/api/users";
+export const authPath = "http://localhost:8080/api/auth";
+
+export const writeMode = "write";
+export const modifyMode = "modify";
